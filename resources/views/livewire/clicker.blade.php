294e1f578c671e7d82c71d8bee270a671139aa35
Vipurthanan <lessandsass@gmail.com>
2025-01-17 08:26:20 +0530

    Data Binding - Sending data from the frontend to the backend
    - Remove everything in the clicker.blade.php file.
    - The input field were created for the name, email and password along with the button.
    - The wire:model attribute was added.
    - The wire:model and wire:click Livewire attributes were added to the fields and button
    - The wire:model attribute was added to the input field.
    - The wire:click attribute was added to the button
    - You can check it by inspecting to the network tab
    - The form is exposing the default behavior but we don’t won’t that actually we are going to make the Ajax request to the server without refreshing the page.

--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,14 +1,27 @@
 <div>
 
-    <h1>{{ $title }}</h1>
-    <h2>{{ $username }}</h2>
+    <form action="" class="m-3 p-3">
 
-    {{-- button --}}
-    <button
-        wire:click="createNewUser"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-        Create new User
-    </button>
+        {{-- input name --}}
+        <div class="mb-2">
+            <input type="text" wire:model="name" placeholder="name" class="border-2 border-gray-700 p-2 rounded-lg">
+        </div>
+
+        {{-- input email --}}
+
+        <div class="mb-2">
+            <input type="email" wire:model="email" placeholder="email" class="border-2 border-gray-700 p-2 rounded-lg">
+        </div>
+
+        {{-- password --}}
+        <div class="mb-2">
+            <input type="password" wire:model="password" placeholder="password" class="border-2 border-gray-700 p-2 rounded-lg">
+        </div>
+
+        <div class="mb-2">
+            <button wire:click.prevent="createNewUser" class="border-2 border-gray-700 p-2 rounded-lg">Create</button>
+        </div>
+
+    </form>
 
 </div>
